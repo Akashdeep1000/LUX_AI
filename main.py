52028402d9e66a8d7bced67b333937e65db86667
Akashdeep1000 <99609618+Akashdeep1000@users.noreply.github.com>
2023-03-21 03:41:15 -0400

    main_skeleton

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
-print("hello world")
+# Import necessary libraries
+import sys
+import os
+from lux.game import Game
+from lux.game_map import GameMap
+from lux.constants import Constants
+from lux.game_constants import GAME_CONSTANTS
+
+# Define your bot class
+class MyBot:
+    def __init__(self):
+        # Add any initializations or variables here
+        pass
+
+    def get_action(self, game_state, unit_commands):
+        # Define your bot's decision making process here
+        pass
+
+# Create a new game object
+game_state = None
+game = Game()
+
+# Start the game loop
+while True:
+    # Get the latest game state
+    try:
+        game_state = game.get_state()
+    except:
+        game.close_window()
+        sys.exit()
+
+    # Check if the game is over
+    if game_state.turn >= Constants.MAX_TURNS:
+        break
+
+    # Get the player's ID
+    player_id = game_state.id
+
+    # Get the game map
+    game_map = game_state.map
+
+    # Create a new instance of your bot
+    bot = MyBot()
+
+    # Get the actions to be taken by your bot
+    actions = bot.get_action(game_state, [])
+
+    # Send the actions to the game engine
+    game.send_actions(actions)
+
+# Close the game window
+game.close_window()
